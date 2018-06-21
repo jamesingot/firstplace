0f8e42e7d139f6640210db51553b519a6189c15f
JamesChell <james.chell@smartrak.co.nz>
2018-06-21 13:32:38 +1200

    CSS 21/06/18

--- public/javascript/popups.js
+++ public/javascript/popups.js
@@ -7,10 +7,10 @@ jQuery.noConflict();
 
         $(function() {
             $(".modal-button").on("click",function() {
-            $("#"+$(this).data("div")).toggle();
+                $("#"+$(this).data("div")).toggle();
             });
-            $(".modal-container").on("click",function() {
-            $(this).closest("div.modal-container").hide();
+            $(".modal-container").click(function() {
+                $(this).closest("div.modal-container").hide();
             });
             
         });
